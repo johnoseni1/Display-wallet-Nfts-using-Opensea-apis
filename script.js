56b8c9503a19b96da56771382a9ada142d9e1181
John Oseni <johnoseni26@gmail.com>
2022-02-21 14:48:45 +0100

    fixed up the bug here

--- script.js
+++ script.js
@@ -27,9 +27,11 @@ const renderTokensForOwner = (ownerAddress) => {
   fetch(
     // this is opensea apis to get it ....
     `https://api.opensea.io/api/v1/assets?owner=${ownerAddress}&order_direction=desc&offset=0&limit=30`,
+
     { method: "GET", headers: { Accept: "application/json" } }
   ).then(response => response.json()).then(({ assets }) => {
     assets.forEach((attributes) => {
+
       document.getElementById("container").append(createTokenElement(attributes))
     })
   })
@@ -39,6 +41,7 @@ const createTokenElement = ({ name, collection, description, permalink, image_pr
   const newElement = document.getElementById("nft_template").content.cloneNode(true)
 
   newElement.querySelector("section").id = `${collection.slug}_${token_id}`
+  
   newElement.querySelector("h1").innerText = name
   newElement.querySelector("a").href = permalink
   newElement.querySelector("img").src = image_preview_url
